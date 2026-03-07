5d858c69fe443084399148fd4999177dd5800b55
madsranden <madsranden95@gmail.com>
2026-03-07 13:55:25 +0100

    Oppdatert rensefil med ny logikk i loopen og fjerning av uteligger

--- rense_data.py
+++ rense_data.py
@@ -3,21 +3,14 @@
 #Importerer pakker
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 import requests
-import os
-from geopy.geocoders import Nominatim
-import time
-import folium
-from geopy.distance import distance
-from scipy.stats import norm
-from scipy import stats
 from barnehage_id import hent_barnehage_oslo_id_str
 
+#Lager tekststreng fra barnehage_id.py
+barnehage_id = hent_barnehage_oslo_id_str()
 
 #Leser data 
-url = "https://statistikkportalen.udir.no/api/rapportering/rest/v1/Statistikk/BHG/FUB/1/3/data?radSti=F&filter=AldergruppeID(-10)_BarnehageenhetID(-12_4932_9901)_BarnehagestoerrelsegruppeID(-10)_KjoennID(-10)_KommunalitetID(-10)_SpoersmaalID(-122_-64_-63_-62_-59_-58_-57_-55_-54_-53)_VisAntallBesvart(1)_VisScore(1)&dataChanged=2026-02-24_125900" #Må legge inn string for riktig id
+url = f"https://statistikkportalen.udir.no/api/rapportering/rest/v1/Statistikk/BHG/FUB/1/3/data?radSti=F&filter=AldergruppeID(-10)_BarnehageenhetID({barnehage_id})_BarnehagestoerrelsegruppeID(-10)_KjoennID(-10)_KommunalitetID(-10)_SpoersmaalID(-122_-64_-63_-62_-59_-58_-57_-55_-54_-53)_VisAntallBesvart(1)_VisScore(1)"
 
 headers = {
     "User-Agent": "Mozilla/5.0",
@@ -44,7 +37,7 @@ rows = data["rows"]
 barnehage_entries = metadata["columns"][4] #Gir liste med dict med barnehagene. Ikke unik så (unike barnehager) * (antall år)
 
 # Antall barnehager per år
-antall_barnehager = len(barnehage_entries) // len(år_liste) #Unike barnehager
+antall_barnehager = len(np.unique([i["name"] for i in barnehage_entries])) #Unike barnehager
 
 barnehager = [
     barnehage_entries[i]["name"]
@@ -54,28 +47,43 @@ barnehager = [
 #Looper over de forskjellige elementene for å kunne lage en df
 list_to_df = []
 
+#Definerer funksjonen som bruker til å håndtere ulike NA
+def parse_snitt(verdi):
+    if not verdi or verdi.strip() in {"*", ""}:
+        return None
+    return float(verdi.replace(",", "."))
+
+
+def parse_antall(verdi):
+    if not verdi or verdi.strip() in {"*", ""}:
+        return None
+    return int(verdi.replace(" ", ""))
+
 for row in rows:
     spørsmål = row["navn"]
     data_values = row["data"]
     
     offset = 0
+    barnehage_offset = 0
     
     for år_index, år in enumerate(år_liste):
         
         år_count = år_column_counts[år_index]# Antall verdier til hvert år
         år_slice = data_values[offset:offset + år_count] #slicer de faktiske verdiene
         offset += år_count #Endrer til loop
-        
-        for i, barnehage in enumerate(barnehager):
-            
+        antall_barnehager_år = år_count // 2 #Git at vi henter snitt  og antall svar
+        # Hent barnehager for dette året
+        barnehager_år = barnehage_entries[barnehage_offset : barnehage_offset + antall_barnehager_år]
+        barnehage_offset += antall_barnehager_år
+
+        for i, barnehage_dict in enumerate(barnehager_år):
+            barnehage = barnehage_dict["name"]
             snitt_idx = i * 2
             antall_idx = snitt_idx + 1
             
-            snitt = år_slice[snitt_idx]
-            antall = år_slice[antall_idx]
+            snitt = parse_snitt(år_slice[snitt_idx])
+            antall = parse_antall(år_slice[antall_idx])
             
-            snitt = float(snitt.replace(",", ".")) if snitt else None
-            antall = int(antall.replace(" ", "")) if antall else None
             list_to_df.append({
                 "år": år,
                 "spørsmål": spørsmål,
@@ -85,3 +93,15 @@ for row in rows:
             })
 
 df = pd.DataFrame(list_to_df)
+
+#Jukser litt. Hver barnehage skal bare ha 10, 20 eller 30 verdier (spm*år)
+terskel = len(år_liste) * len(rows) # Setter terskelen som det ikke være flere verdier enn
+
+rare_barnehager = (
+    df.groupby("barnehage")
+      .size()
+      .loc[lambda x: x > terskel]
+      .index
+)
+
+df_ren = df[~df["barnehage"].isin(rare_barnehager)]
